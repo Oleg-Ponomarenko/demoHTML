617d41a8b16cf475c52c59bce7ed85e87c75c7ca
ponomarenkoog.22 <ponomarenko1711@mail.ru>
2021-12-22 12:48:23 +0300

    Initial commit

---
+++ src/test/java/maingroup/demohtml/DemoHtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package maingroup.demohtml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHtmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
